1d3cb4116529f9116bddd75ca5a2ba28461ad01c
IGOR SILVA DE BRITO <igor.sbrito1@senacsp.edu.br>
2026-06-17 21:55:18 -0300

    atividade de media

---
+++ ex.06/script.js
@@ -0,0 +1,32 @@
+// Escreva um programa que receba 3 notas, calcule a media e se madia > 7= aprovado, abaixo reprovado
+// Ajuste o sistema para receber o nome do aluno, as 3 notas e  a media no final 
+
+let nome = prompt("Digite o nome do aluno:");
+
+let nota1 = parseFloat(prompt("Digite a primeira nota:"));
+let nota2 = parseFloat(prompt("Digite a segunda nota:"));
+let nota3 = parseFloat(prompt("Digite a terceira nota:"));
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+if (media >= 7) {
+    alert(
+        `Aluno: ${nome}
+Nota 1: ${nota1}
+Nota 2: ${nota2}
+Nota 3: ${nota3}
+Média: ${media}
+
+Situação: Aprovado`
+    );
+} else {
+    alert(
+        `Igor: ${nome}
+Nota 1: ${nota1}
+Nota 2: ${nota2}
+Nota 3: ${nota3}
+Média: ${media}
+
+Situação: Reprovado`
+    );
+}
